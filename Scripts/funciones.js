0edb3c19a0c4a7664af0f2ef18889bbd70f9d68a
Stiven Ortiz <stiven.ortiz@uao.edu.co>
2025-08-27 20:02:18 -0500

    Primer commit

---
+++ Scripts/funciones.js
@@ -0,0 +1,14 @@
+
+
+function mostrarResultados(name, lenguaje){
+    return `${name} Seleccionaste ${lenguaje}`;
+}
+
+export {mostrarResultados};
+
+//Numero total de votos por opción//
+
+
+
+//Porcentaje de paticipación por opción//
+
---
+++ Scripts/main.js
@@ -0,0 +1,43 @@
+
+
+//import {mostrarResultados} from './funciones';
+
+import { mostrarResultados } from "./Funciones";
+
+const btnVotar = document.getElementById("votar");
+const btnResultados = document.getElementById("mostrar");
+
+btnEjecutar.addEventListener('click',ingresarVoto)
+btnRecordar.addEventListener('click',desplegarTodos)
+
+let historial = [];
+
+let info = ''
+
+let tARespuesta = document.getElementById("laRespuesta");
+
+function ingresarVoto(){
+    let nombre = document.getElementById("elNombre").value;   
+    let lenguaje = parseInt(document.getElementById("elLenguaje").value);
+    let res
+
+    if (nombre.length==0 || isNaN(lenguaje)) {
+        res = 'El nombre o el lenguaje, no fueron ingresados o tienen valores de entrada errados'
+
+        document.getElementById("error").innerHTML = res;
+        console.log(res);
+
+    }else{
+        document.getElementById("error").innerHTML = "";
+        res = mostrarResultados(nombre, lenguaje)
+        info +=  res +'\n'; 
+        tARespuesta.textContent = res   
+    }
+
+    
+}
+
+function desplegarTodos()
+{
+    tARespuesta.textContent = info;
+}
